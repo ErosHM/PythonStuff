79afe3685cdabf377a974f244caa5a0750a387f0
ErosHM <eros-hm@hotmail.com>
2021-08-06 20:40:01 -0500

    Agregado nuevo inteno de LOL cat factory

---
+++ lolcat_factory/program.py
@@ -0,0 +1,34 @@
+def get_or_create_output_folder():
+    pass
+
+
+def show_cats():
+    pass
+
+
+def download_cats():
+    pass
+
+
+def print_header():
+    pass
+
+
+def main():
+    # imprimir la cabecera
+    print_header()
+
+    # obtener o crear el folder de salida
+    get_or_create_output_folder()
+
+    # descargar gatos
+    download_cats()
+
+    # mostrar gatos
+    show_cats()
+
+    print("Hola desde el main")
+
+
+if __name__ == '__main__':
+    main()
